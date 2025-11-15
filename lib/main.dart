9a9f4417b846d666a574098aa0f8973dc284e583
Katsarski <boyankatsarski@gmail.com>
2025-11-15 22:21:56 +0200

    portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import 'package:flutter/services.dart';
 import 'amplifyconfiguration.dart';
 import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await _configureAmplify();
+
+  // Force portrait only
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const BabhDnevniciteApp());
 }
 
